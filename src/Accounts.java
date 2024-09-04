1b8ad4891808243ff91e3f6b638b823ed52ac803
jopiajay23@gmail.com <christopherechavez13@gmail.com>
2024-09-04 14:31:07 +0800

    ongoing account and accounts

--- src/Accounts.java
+++ src/Accounts.java
@@ -1,13 +1,112 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.Scanner;
 
-/**
- *
- * @author SCC-COLLEGE
- */
 public class Accounts {
-    
+    Scanner sc = new Scanner(System.in);
+    Account[] acc = new Account[100];
+
+    public void manageAccounts() {
+        int no, i, id;
+
+        System.out.print("Enter number of Accounts: ");
+        no = sc.nextInt();
+
+        for (i = 0; i < no; i++) {
+            acc[i] = new Account();
+
+            boolean identi;
+            boolean email1;
+            boolean username;
+
+            System.out.println("Enter account Details " + (i + 1) + ": ");
+            while (true) {
+                System.out.print("Enter ID: ");
+                id = sc.nextInt();
+                identi = false;
+
+                for (int x = 0; x < i; x++) {
+                    if (acc[x] != null && acc[x].id == id) {
+                        identi = true;
+                        break;
+                    }
+                }
+
+                if (identi) {
+                    System.out.println(" - ID already exists. Try again -");
+                } else {
+                    acc[i].id = id;
+                    break;
+                }
+            }
+
+            System.out.print("First Name: ");
+            String fname = sc.next();
+            System.out.print("Last Name: ");
+            String lname = sc.next();
+
+            while (true) {
+                System.out.print("Email: ");
+                String email = sc.next();
+                email1 = false;
+
+                for (int x = 0; x < i; x++) {
+                    if (acc[x] != null && acc[x].email.equals(email)) {
+                        email1 = true;
+                        break;
+                    }
+                }
+
+                if (email1) {
+                    System.out.println(" - Email already exists. Try again -");
+                } else {
+                    acc[i].email = email;
+                    break;
+                }
+            }
+
+            while (true) {
+                System.out.print("Username: ");
+                String user = sc.next();
+                username = false;
+
+                for (int x = 0; x < i; x++) {
+                    if (acc[x] != null && acc[x].username.equals(user)) {
+                        username = true;
+                        break;
+                    }
+                }
+
+                if (username) {
+                    System.out.println(" - Username already exists. Try again -");
+                } else {
+                    acc[i].username = user;
+                    break;
+                }
+            }
+
+            boolean passValid = false;
+
+            while (!passValid) {
+                System.out.print("Password: ");
+                String password = sc.next();
+
+                if (acc[i].passwordVerify(password)) {
+                    acc[i].addAccount(acc[i].id, fname, lname, acc[i].email, acc[i].username, password);
+                    passValid = true;
+                } else {
+                    System.out.println(" - Invalid password - , Try Again ");
+                }
+            }
+            System.out.println("");
+        }
+
+        System.out.println("Account Details: ");
+        System.out.printf("%-10s %-10s %-10s %-20s %-13s %-10s\n", "ID", "FirstName", "LastName", "Email", "Username", "Password");
+        System.out.println("---------------------------------------------------------------------------------");
+        for (i = 0; i < no; i++) {
+            if (acc[i] != null) {
+                acc[i].viewAccount();
+                System.out.println("");
+            }
+        }
+    }
 }
